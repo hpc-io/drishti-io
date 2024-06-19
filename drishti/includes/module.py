49714e674f133eb349040335904d818b134c63fc
Joel Tony <github@jaytau.com>
2024-06-19 12:33:19 +0530

    fmt: import statements

--- drishti/includes/module.py
+++ drishti/includes/module.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-import csv
 import datetime
+import csv
 import time
-
 import pandas as pd
 from rich import box
 from rich.syntax import Syntax
-
-from rich.syntax import Syntax
-
 from drishti.includes.config import *
 
 '''
@@ -1907,7 +1903,7 @@ def export_csv(export_dir, filename, jobid=None):
 
     
     os.makedirs(export_dir, exist_ok=True) # Ensure export directory exists
-    filepath = os.path.join(export_dir, f"{filename}-summary.csv")
+    filepath = os.path.join(export_dir, f"{filename}.csv")
 
     with open(filepath, 'w') as f:
         w = csv.writer(f)
