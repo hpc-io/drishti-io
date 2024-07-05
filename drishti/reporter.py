19bdf0a8890c800360d5280ff5abe9dfb1804a95
Joel Tony <github@jaytau.com>
2024-07-05 12:58:16 +0530

    feat: check_log_type() processes multiple traces

--- drishti/reporter.py
+++ drishti/reporter.py
@@ -27,23 +27,45 @@ def clear():
     _ = call('clear' if os.name == 'posix' else 'cls')
 
 
-def check_log_type(path):
-    if path.endswith('.darshan'):
-        if not os.path.isfile(path):
-            print('Unable to open .darshan file.')
-            sys.exit(os.EX_NOINPUT)
-        else:
+def check_log_type(paths: list[str]) -> int | None:
+    is_darshan = True
+    is_recorder = True
+    multiple_logs = len(paths) > 1
+
+    for path in paths:
+        if path.endswith('.darshan'):
+            if not os.path.isfile(path):
+                print('Unable to open .darshan file.')
+                sys.exit(os.EX_NOINPUT)
+            else:
+                is_darshan = True and is_darshan
+                is_recorder = False and is_recorder
+        else:  # check whether is a valid recorder log
+            if not os.path.isdir(path):
+                print('Unable to open recorder folder.')
+                sys.exit(os.EX_NOINPUT)
+            else:
+                is_recorder = True and is_recorder
+                is_darshan = False and is_darshan
+
+    if multiple_logs:
+        if is_darshan:
             return LOG_TYPE_DARSHAN
-    else:  # check whether is a valid recorder log
-        if not os.path.isdir(path):
-            print('Unable to open recorder folder.')
-            sys.exit(os.EX_NOINPUT)
         else:
+            print('Only .darshan files are supported for multiple logs.') #TODO
+            sys.exit(os.EX_NOINPUT)
+    else:
+        if is_darshan and not is_recorder:
+            return LOG_TYPE_DARSHAN
+        elif is_recorder and not is_darshan:
             return LOG_TYPE_RECORDER
+        else:
+            print('Unable to reliably determine the log type.')
+            sys.exit(os.EX_NOINPUT)
 
 
 def main():
-    log_type = check_log_type(args.log_path)
+    log_type = check_log_type(args.log_paths)
 
     if log_type == LOG_TYPE_DARSHAN:
         from drishti.handlers.handle_darshan import handler
