0827404f8f0d5e8272428c378fe35575381cbee5
ujjwx1 <uaggarwal1_be23@thapar.edu>
2026-03-05 00:34:57 +0530

    fix: handle non-existent paths and unsupported formats separately
    Previously, both missing paths and unsupported file formats showed
    the same misleading error. Now distinguishes between:
    - Path does not exist: clear not-found error
    - Path exists but wrong format: unsupported format error
    Also removed trailing whitespace after else clause.

--- drishti/reporter.py
+++ drishti/reporter.py
@@ -34,10 +34,12 @@ def check_log_type(path):
         if not os.path.isfile(path):
             print('Unable to open .darshan file.')
             sys.exit(os.EX_NOINPUT)
-        else: 
+        else:
             return LOG_TYPE_DARSHAN
     elif os.path.isdir(path):
         return LOG_TYPE_RECORDER
+    elif not os.path.exists(path):
+        sys.exit('Error: Path does not exist: {}'.format(path))
     else:
         print('Unsupported file format. Please provide a .darshan file or a Recorder directory.')
         sys.exit(1)
